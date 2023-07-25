9654bb3e3461428219e20f2aa3b8e458de01ddbd
ouegy <ianbutler82@yahoo.co.uk>
2023-07-25 17:45:07 +0100

    added initialise game function to fix the first round bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("It reads the .js file");
-let playerChoice = "";
-let computerChoice = "";
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomInt = getRandomInt(3);
@@ -31,44 +31,82 @@ function getRandomInt(max) {
 
 function getPlayerChoice() {
     playerChoice = prompt("Enter your choice. Either: Rock, Paper or Scissors");
-    switch (playerChoice.toLowerCase()) {
-        case "rock":
-            console.log(`Player chose ${playerChoice}`);
-            break;
-        case "paper":
-            console.log(`Player chose ${playerChoice}`);
-            break; 
-        case "scissors":
-            console.log(`Player chose ${playerChoice}`);
-            break;      
-        default:
-            alert("That is not a valid choice, please try again!");
-            console.log("That is not a valid choice, please try again!");
-            getPlayerChoice();
-            break;
+    if (playerChoice === null) {
+        console.log("The player left the game");
+        return;
     }
+    else if (playerChoice != null) {
+        switch (playerChoice.toLowerCase()) {
+            case "rock":
+                console.log(`Player chose ${playerChoice}`);
+                break;
+            case "paper":
+                console.log(`Player chose ${playerChoice}`);
+                break; 
+            case "scissors":
+                console.log(`Player chose ${playerChoice}`);
+                break;             
+            default:
+                alert("That is not a valid choice, please try again!");
+                console.log("That is not a valid choice, please try again!");
+                break;
+        }  
+    }
+    
     console.log(playerChoice);
-    typeof playerChoice;
+    console.log(typeof playerChoice);
     return playerChoice;
-
 }
 
 function playRound(computerChoice, playerChoice) {
+
+    // computerChoice = getComputerChoice();
     if (playerChoice === computerChoice) {
         console.log("The game is a draw!");
+        console.log(playerScore);
+        console.log(computerScore);
     }
-    else if (playerChoice == "rock" && computerChoice == "scissors"
-        || playerChoice == "paper" && computerChoice == "rock"
-        || playerChoice == "scissors" && computerChoice == "paper") {
+    else if ( (playerChoice == "rock" && computerChoice == "scissors")
+        || (playerChoice == "paper" && computerChoice == "rock")
+        || (playerChoice == "scissors" && computerChoice == "paper") ) {
             console.log("Player wins!");
+            playerScore++;
+            console.log(playerScore);
+            console.log(computerScore);
         }
-    else if (playerChoice == "rock" && computerChoice == "paper"
-    || playerChoice == "paper" && computerChoice == "scissors"
-    || playerChoice == "scissors" && computerChoice == "rock") {
+    else if ( (playerChoice == "rock" && computerChoice == "paper")
+    || (playerChoice == "paper" && computerChoice == "scissors")
+    || (playerChoice == "scissors" && computerChoice == "rock") ) {
         console.log("Computer wins!");
+        computerScore++;
+        console.log(playerScore);
+        console.log(computerScore);
     }
 }
 
-getComputerChoice();
-getPlayerChoice();
-playRound(computerChoice, playerChoice);
+function game() {
+    initialiseGame();
+    while (playerScore < 5 && computerScore < 5) {
+        initialiseGame();
+        console.log(playerScore);
+        console.log(computerScore);
+        if (playerScore == 5) {
+            console.log("Player wins the best of 5!");
+            break;
+        }
+        else if (computerScore == 5) {
+            console.log("Computer wins the best of 5!");
+            break;
+        }
+    }
+}
+function initialiseGame() {
+    getComputerChoice();
+    getPlayerChoice();
+    playRound(computerChoice, playerChoice);
+}
+game();
+// I can only pass the arguments in to the playRound() function after getComputerChoice() and getPlayerChoice()
+// getComputerChoice();
+// getPlayerChoice();
+// playRound(computerChoice, playerChoice);
