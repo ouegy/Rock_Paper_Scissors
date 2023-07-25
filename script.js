dc41a39ec3af2d4e03b42ff506b3627015db47e0
ouegy <ianbutler82@yahoo.co.uk>
2023-07-25 13:41:30 +0100

    Fixed the prompt dialog bug

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
-let playerScore = 0;
-let computerScore = 0;
-console.log(playerScore);
-console.log(computerScore);
+console.log("It reads the .js file");
+let playerChoice = "";
+let computerChoice = "";
 
 function getComputerChoice() {
     let randomInt = getRandomInt(3);
@@ -24,6 +22,7 @@ function getComputerChoice() {
     }
     console.log(computerChoice);
     console.log(typeof computerChoice);
+    return computerChoice;
 }
 
 function getRandomInt(max) {
@@ -32,75 +31,44 @@ function getRandomInt(max) {
 
 function getPlayerChoice() {
     playerChoice = prompt("Enter your choice. Either: Rock, Paper or Scissors");
-    if (playerChoice === null) {
-        console.log("The player left the game");
-        return;
+    switch (playerChoice.toLowerCase()) {
+        case "rock":
+            console.log(`Player chose ${playerChoice}`);
+            break;
+        case "paper":
+            console.log(`Player chose ${playerChoice}`);
+            break; 
+        case "scissors":
+            console.log(`Player chose ${playerChoice}`);
+            break;      
+        default:
+            alert("That is not a valid choice, please try again!");
+            console.log("That is not a valid choice, please try again!");
+            getPlayerChoice();
+            break;
     }
-    else if (playerChoice != null) {
-        switch (playerChoice.toLowerCase()) {
-            case "rock":
-                console.log(`Player chose ${playerChoice}`);
-                break;
-            case "paper":
-                console.log(`Player chose ${playerChoice}`);
-                break; 
-            case "scissors":
-                console.log(`Player chose ${playerChoice}`);
-                break;             
-            default:
-                alert("That is not a valid choice, please try again!");
-                console.log("That is not a valid choice, please try again!");
-                //getPlayerChoice();
-                break;
-        }  
-    }
-    
     console.log(playerChoice);
-    console.log(typeof playerChoice);
+    typeof playerChoice;
+    return playerChoice;
 
 }
 
 function playRound(computerChoice, playerChoice) {
-    getPlayerChoice();
-    getComputerChoice();
     if (playerChoice === computerChoice) {
         console.log("The game is a draw!");
-        roundResult = "draw";
     }
-    else if ( (playerChoice == "rock" && computerChoice == "scissors")
-        || (playerChoice == "paper" && computerChoice == "rock")
-        || (playerChoice == "scissors" && computerChoice == "paper") ) {
+    else if (playerChoice == "rock" && computerChoice == "scissors"
+        || playerChoice == "paper" && computerChoice == "rock"
+        || playerChoice == "scissors" && computerChoice == "paper") {
             console.log("Player wins!");
-            roundResult = "player";
-            playerScore++;
         }
-    else if ( (playerChoice == "rock" && computerChoice == "paper")
-    || (playerChoice == "paper" && computerChoice == "scissors")
-    || (playerChoice == "scissors" && computerChoice == "rock") ) {
+    else if (playerChoice == "rock" && computerChoice == "paper"
+    || playerChoice == "paper" && computerChoice == "scissors"
+    || playerChoice == "scissors" && computerChoice == "rock") {
         console.log("Computer wins!");
-        roundResult = "computer";
-        computerScore++;
     }
 }
 
-function game() {
-    getPlayerChoice();
-    getComputerChoice();
-    while (playerScore < 5 && computerScore < 5) {
-        playRound(computerChoice, playerChoice);
-        console.log(playerScore);
-        console.log(computerScore);
-    }
-    if (playerScore == 5) {
-        console.log("Player wins the best of 5!");
-    }
-    else if (computerScore == 5) {
-        console.log("Computer wins the best of 5!");
-    }
-}
-
-// getComputerChoice();
-// getPlayerChoice();
-
-
-game();
+getComputerChoice();
+getPlayerChoice();
+playRound(computerChoice, playerChoice);
